d883ffdc56957904002cca865554b06db1efde6a
Albert <jinag2@gmail.com>
2017-03-22 11:45:52 +0800

    no message

--- MergeCSV.py
+++ MergeCSV.py
@@ -1,1 +1,38 @@
 #! python3
+
+import csv
+import sys
+import os
+import traceback
+
+
+def main():
+    target_csv_list = read_csv(sys.argv[1])
+    x = len(target_csv_list)
+
+
+def read_csv(filename):
+    try:
+        csv_file = open(filename)
+    except:
+        print(traceback.format_exc())
+        sys.exit()
+    csv_reader = csv.reader(csv_file)
+    return list(csv_reader)
+
+
+def abstract_float_col(data_list, col):
+    value_list = []
+    for row in data_list:
+        value_list.append(float(row[col]))
+    return value_list
+
+
+def abstract_int_col(data_list, col):
+    value_list = []
+    for row in data_list:
+        value_list.append(int(row[col]))
+    return value_list
+
+
+main()
